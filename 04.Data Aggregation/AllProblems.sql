db953e8ad9318a746ca721231f21b19c6d5a21a9
Jeni <jeni241093@gmail.com>
2017-09-30 21:51:36 +0300

    DataAggregation

---
+++ 04.Data Aggregation/AllProblems.sql
@@ -0,0 +1,39 @@
+---Problem 1.	Records’ Count
+SELECT COUNT(Id) AS Count
+FROM WizzardDeposits;
+
+---Problem 2.	Longest Magic Wand
+SELECT*FROM  WizzardDeposits
+SELECT MAX(MagicWandSize) AS LongestMagicWand
+FROM WizzardDeposits
+---Problem 3.	Longest Magic Wand per Deposit Groups
+SELECT DepositGroup,
+MAX (MagicWandSize) AS LongestMagicWand
+FROM WizzardDeposits
+GROUP BY DepositGroup
+
+---Problem 4.Smallest Deposit Group per Magic Wand Size
+SELECT TOP(2)
+WITH TIES DepositGroup
+FROM WizzardDeposits
+GROUP BY DepositGroup
+ORDER BY AVG(MagicWandSize)
+
+---Problem 5.	Deposits Sum
+SELECT DepositGroup, SUM(DepositAmount)AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup
+---Problem 6.	Deposits Sum for Ollivander Family
+SELECT DepositGroup, SUM(DepositAmount)AS TotalSum
+FROM WizzardDeposits
+WHERE MagicWandCreator ='Ollivander family'
+GROUP BY DepositGroup
+---Problem 7.	Deposits Filter
+SELECT DepositGroup, SUM(DepositAmount)AS TotalSum
+FROM WizzardDeposits
+WHERE MagicWandCreator ='Ollivander family'
+GROUP BY DepositGroup
+HAVING SUM(DepositAmount)<150000
+ORDER BY SUM(DepositAmount)DESC
+
+---Problem 8.Deposit Charge
